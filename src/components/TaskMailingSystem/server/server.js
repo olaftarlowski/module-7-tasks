af00d8e8a7e0c5c5077f4b831e99d6a70c7f514d
OlfiDell <bobr.rozrabiaka123@gmail.com>
2022-07-18 19:43:14 +0200

    mailgun starter

---
+++ src/components/TaskMailingSystem/server/server.js
@@ -0,0 +1,27 @@
+import Mailgun from "mailgun.js";
+import formData from "form-data";
+
+const API_KEY = "04ce4da26c31c2f04c313e8844fa05bf-787e6567-41b6b781";
+const DOMAIN = "sandboxa486d9dc73e141618d0ea407b3dfe173.mailgun.org";
+
+// const formData = require("form-data");
+// const Mailgun = require("mailgun.js");
+
+const mailgun = new Mailgun(formData);
+const client = mailgun.client({ username: "api", key: API_KEY });
+
+const messageData = {
+  from: "Excited User <me@samples.mailgun.org>",
+  to: "bobr.rozrabiaka123@gmail.com",
+  subject: "Hello",
+  text: "Testing some Mailgun awesomeness!",
+};
+
+client.messages
+  .create(DOMAIN, messageData)
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.error(err);
+  });
