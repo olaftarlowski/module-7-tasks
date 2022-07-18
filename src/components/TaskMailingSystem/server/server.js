cd5232bd6fd5637891320d0ad8a8f65dd9992c2b
OlfiDell <bobr.rozrabiaka123@gmail.com>
2022-07-18 23:29:23 +0200

    ch

--- src/components/TaskMailingSystem/server/server.js
+++ src/components/TaskMailingSystem/server/server.js
@@ -1,7 +1,7 @@
 import Mailgun from "mailgun.js";
 import formData from "form-data";
-const API_KEY = process.env.REACT_APP_MAILGUN_API_KEY;
 
+const API_KEY = process.env.REACT_APP_MAILGUN_API_KEY;
 const DOMAIN = "sandboxa486d9dc73e141618d0ea407b3dfe173.mailgun.org";
 
 const mailgun = new Mailgun(formData);
